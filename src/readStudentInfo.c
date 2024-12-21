21349503839cb91c6385299069542f0640bbdeec
Tsun-Chi Wong <xhzzzly@126.com>
2024-12-21 20:53:06 +0800

    add read function

---
+++ src/readStudentInfo.c
@@ -0,0 +1,34 @@
+#include "student.h"
+#include "bTree.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int ReadStudentInfo(const char* filename, BTree* bTree) {
+    FILE *fp;
+
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("无法打开文件\n");
+        exit(-1);
+    }
+
+    // 读取学生信息
+    int id;
+    int num = 0;
+    while (fscanf(fp, "%d", &id) != EOF) {
+        ++num;
+        char name[NAME_STRING_LENGTH];
+        int birth[3];
+        double scores[3];
+        fscanf(fp, "%s %d %d %d %lf %lf %lf", name, &birth[0], &birth[1], &birth[2], &scores[0], &scores[1], &scores[2]);
+        Student student = createStudent(name, id, birth, scores);
+        insert(bTree, student);
+    }
+
+    fclose(fp);
+    return num;
+}
+
+
---
+++ src/readStudentInfo.h
@@ -0,0 +1,8 @@
+#ifndef READ_STUDENT_INFO_H
+#define READ_STUDENT_INFO_H
+
+#include "bTree.h"
+
+extern int ReadStudentInfo(const char* filename, BTree* bTree);
+
+#endif
